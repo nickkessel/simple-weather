4b09987e383bb7fab4a961bb47365c17c10db04f
Nick Kessel <80365598+nickkessel@users.noreply.github.com>
2021-05-28 13:38:47 -0400

    more changes

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ function getWeather() {
   let feels = document.getElementById("feels");
   let location = document.getElementById("location");
   let minmax = document.getElementById("minmax");
+  let stats = document.getElementById("stats");
+  let body = document.getElementById("body");
 
   let api = "https://api.openweathermap.org/data/2.5/weather";
   let apiKey = "9eb845f6ddd80a3a7314e134d5c9facb";
@@ -30,9 +32,17 @@ function getWeather() {
       .then(data => {
         console.log(data);
         let temp = data.main.temp;
+        let cond = data.weather[0].main;
         temperature.innerHTML = "Right now, in "+ data.name + ", it is " + Math.round(temp) + "° F. It feels like " + Math.round(data.main.feels_like) + "° F.";
-        description.innerHTML = data.weather[0].main;
-        minmax.innerHTML = "The high today is " + data.main.temp_max + ", and the low is " + data.main.temp_min + ".";
+        description.innerHTML = cond;
+        minmax.innerHTML = "The high today is " + Math.round(data.main.temp_max) + ", and the low is " + Math.round(data.main.temp_min) + ".";
+        stats.innerHTML = "Humidity: " + data.main.humidity + "%" + "<br>" + "Pressure: " + data.main.pressure + "mb" + "<br>" + "Wind speed: " + Math.round(data.wind.speed) + " mph" + "<br>" + "Wind gusts: " + Math.round(data.wind.gust) + " mph";
+        if(cond = "Clouds") {
+          document.getElementById("body").style.background= "silver";
+        }
+        else if(cond = "Rain") {
+          document.getElementById("body").style.background = "blue";
+        }
       });
   }
   function error() {
