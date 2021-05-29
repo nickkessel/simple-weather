866c08380b92ff1d7bb05e4ffa4241781b390be9
Nick Kessel <80365598+nickkessel@users.noreply.github.com>
2021-05-29 15:01:43 -0400

    check desc
    ok so i think the fix i put out yesterday worked? IT HAS BEEN CLOUDY FOR AN ENTIRRE DAY IC ANT TEST IT

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ function getWeather() {
   let stats = document.getElementById("stats");
   let body = document.getElementById("body");
 
-  let api = "https://api.openweathermap.org/data/2.5/weather";
+  let api = "https://api.openweathermap.org/data/2.5/onecall";
   let apiKey = "9eb845f6ddd80a3a7314e134d5c9facb";
 
   navigator.geolocation.getCurrentPosition(success, error);
@@ -25,25 +25,50 @@ function getWeather() {
     longitude +
     "&appid=" +
     apiKey +
-    "&units=imperial";
+    "&units=imperial"
+    ;
 
     fetch(url)
       .then(response => response.json())
       .then(data => {
         console.log(data);
-        let temp = data.main.temp;
-        let cond = data.weather[0].main;
-        temperature.innerHTML = "Right now, in "+ data.name + ", it is " + Math.round(temp) + "° F. It feels like " + Math.round(data.main.feels_like) + "° F.";
-        description.innerHTML = cond;
-        minmax.innerHTML = "The high today is " + Math.round(data.main.temp_max) + ", and the low is " + Math.round(data.main.temp_min) + ".";
-        stats.innerHTML = "Humidity: " + data.main.humidity + "%" + "<br>" + "Pressure: " + data.main.pressure + "mb" + "<br>" + "Wind speed: " + Math.round(data.wind.speed) + " mph" + "<br>" + "Wind gusts: " + Math.round(data.wind.gust) + " mph";
-        if(cond = "Clouds") {
+        //current vars
+        let c_temp = data.current.temp;
+        let c_cond = data.current.weather[0].main;
+        let c_feels = data.current.feels_like;
+        let c_pressure = data.current.pressure;
+        let c_uv = data.current.uvi;
+        let c_cloudcover = data.current.clouds;
+        let c_humid = data.current.humidity;
+        let c_wind = data.current.wind_speed;
+        let c_windgust = data.current.wind_gust;
+        let c_dewpoint = data.current.dew_point;
+
+        //hourly vars
+
+
+        //daily vars
+        let d_max0 = data.daily[0].temp.max;
+        let d_min0 = data.daily[0].temp.min;
+
+        temperature.innerHTML = "Right now, it is " + Math.round(c_temp) + "°. It feels like " + Math.round(c_feels) + "°.";
+        description.innerHTML = c_cond;
+        minmax.innerHTML = "The high today is " + Math.round(d_max0) + ", the low is " + Math.round(d_min0) + ", and the dew point is " + Math.round(c_dewpoint) + ".";
+        stats.innerHTML =
+        "Humidity: " + c_humid + "%" + "<br>"
+         + "Pressure: " + c_pressure + "mb" + "<br>"
+         + "Wind speed: " + Math.round(c_wind) + " mph" + "<br>"
+         + "Wind gusts: " + Math.round(c_windgust) + " mph" + "<br>"
+         + "Cloud cover: " + c_cloudcover + "%" + "<br>"
+         + "UV Index: " + Math.round(c_uv) + "<br>"
+
+         ;
+        if(c_cond = "Clouds") {
           body.style.background= "silver";
-        } else if(cond = "Rain") {
+        } else if(c_cond = "Rain") {
           body.style.background = "lightskyblue";
-        }
-        else {
-          body.style.backgroun = "red";
+        } else {
+          body.style.background = "red";
         }
       });
   }
@@ -51,4 +76,15 @@ function getWeather() {
     console.log("error");
   }
 }
+
+
+
+
 getWeather()
+
+/*INFO time!
+a "c" before a variable means current. example:"c_temp, c_cond"
+a "h" before a variable means hourly. example:"h[0]_temp, h[0]_cond" the number in brackets is the amount of hours out the forecast is
+a "d" before a variable means daily. example:"d[0]_temp, d[0]_cond" the number in brackets is the amount of days out the forecast is
+
+*/
